929cbf8f1abe1abbed4e81a08a9e975664ffb267
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-05-23 13:12:56 -0700

    c08-ch04-Trigger_Completed

--- Advanced PostgreSQL Course/3_Triggers/ch03_2-Creating_Row-Level_After_Update_Trigger.sql
+++ Advanced PostgreSQL Course/3_Triggers/ch03_2-Creating_Row-Level_After_Update_Trigger.sql
@@ -1,6 +1,5 @@
 -- Now let's create a trigger when a value in a table is updated
 -- Create another table for auditing salary increment
-
 CREATE TABLE employee_salary_logs (
    id INT GENERATED ALWAYS AS IDENTITY,
    first_name TEXT NOT NULL,
@@ -12,7 +11,6 @@ CREATE TABLE employee_salary_logs (
 
 -- Now let's create a function
 -- If the salary is incremented, then let's aduit the last name and the time we incremented the salary and what the old salary and new salary is
-
 CREATE OR REPLACE FUNCTION employee_salary_update_func()
   RETURNS TRIGGER
   LANGUAGE PLPGSQL
@@ -30,20 +28,16 @@ $$
 
 
 -- Let's create the trigger
-
 CREATE TRIGGER employee_salary_update_trigger
-  AFTER UPDATE
-  ON employees
+  AFTER UPDATE ON employees
   FOR EACH ROW
   EXECUTE PROCEDURE employee_salary_update_func();
 
 
 -- Now let's view the employees table entires
-
 SELECT * FROM employees;
 
 -- Let's update the salary of an employee
-
 UPDATE employees
 SET salary = 75000
 WHERE last_name = 'Watson'; 
@@ -53,17 +47,14 @@ SELECT * FROM employees;
 
 -- We see the value is changed
 -- Now let's observe the audits table
-
 SELECT * FROM employee_salary_logs;
 
 -- We see the old salary, new salary and the time on which we changed the salary
 
 
 -- One more update, increment all salaries by 10%
-
 UPDATE employees
 SET salary = 1.1 * salary
 
 -- Again check (there should be a total of 4 entries, 2 for Watson)
-
 SELECT * FROM employee_salary_logs;
