8ac2a6b569a65d4c237b3e484c83cefcda2ca4b8
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-06-21 11:46:27 -0700

    c38-db_cert_build001

---
+++ db_project_certDesign/db_build_emp_cert.sql
@@ -0,0 +1,66 @@
+SELECT *
+FROM pg_catalog.pg_tables
+WHERE schemaname != 'pg_catalog' AND
+    schemaname != 'information_schema';
+
+--keep certification for test, this table and schema will be removed
+select * from certification.certificate;
+
+-- cleare tables and schema
+DROP TABLE IF EXISTS certificate.skill_lines;
+DROP TABLE IF EXISTS certificate.skills;
+DROP TABLE IF EXISTS certificate.tutor_lines;
+DROP TABLE IF EXISTS certificate.tutors;
+DROP TABLE IF EXISTS certificate.platforms;
+DROP TABLE IF EXISTS certificate.org_lines;
+DROP TABLE IF EXISTS certificate.orgs;
+DROP TABLE IF EXISTS certificate.certificates;
+DROP SCHEMA IF EXISTS certificate;
+DROP TABLE IF EXISTS employee.employees;
+DROP SCHEMA IF EXISTS employee;
+
+
+-- Create the database schemas
+CREATE SCHEMA employee;
+CREATE SCHEMA certificate;
+
+
+-- Create a table for the db_portfolio - certificates
+CREATE TABLE certificate.certificates (
+    cert_id         INT GENERATED ALWAYS AS IDENTITY (START WITH 100 INCREMENT BY 1) NOT NULL PRIMARY KEY,
+    cert_name       VARCHAR(255) NOT NULL,
+    completed_date  DATE,
+    course_length   TIME,
+    study_time      TIME,
+    skill_id        INT,
+    platform_id     INT,
+    issuing_org_id  INT, 
+    showcase        BOOLEAN,
+    cert_code       VARCHAR(255),
+    cloud_path      VARCHAR(255),
+    exam_score      INT,
+    hands_on        BOOLEAN,
+    repo            VARCHAR(255),
+    posted_linkedin TIMESTAMP,
+    tutor_id        INT,
+    note            VARCHAR(50)
+);
+
+-- Create a table for the db_portfolio -  skills data
+CREATE TABLE certificate.skills (
+    skill_id    INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
+    skill_name  VARCHAR(50) NOT NULL,
+    skill_description VARCHAR(50)
+);
+
+-- Create a table for the db_portfolio - skill_lines
+CREATE TABLE certificate.skill_lines (
+    skill_line_id   INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
+    cert_id         INT,
+    skill_id        INT
+);
+
+select * from certificate.skills where skill_name ~* 'python';
+delete from certificate.skills where skill_name ~* ' python';
+delete from certificate.skills where skill_name = 'Python (Programming Language';
+select * from certificate.skills;
